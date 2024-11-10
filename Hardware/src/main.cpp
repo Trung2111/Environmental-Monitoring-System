101baa3c518c777cbf1a5686d7a52eac93674f5e
Trung2111 <trungnm.b21dt226@stu.ptit.edu.vn>
2024-11-10 22:19:39 +0700

    1

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -1,34 +1,22 @@
-// #include <Arduino.h>
-// #include <Ticker.h>
-// #include "mqtt.h"
+#include <Arduino.h>
+#include <Ticker.h>
+#include "mqtt.h"
 
-// Ticker ticker;
+Ticker ticker;
 
-// void setup() {
-//   Serial.begin(115200);
-//   BH1750FVI_Init();
-//   DHT11_Init();
-//   Mqtt_Init();
-//   if (!client.connected()) {
-//     Mqtt_Reconnect();
-//   }
-//   ticker.attach(10.0, MqttSend);
-// }
-
-// void loop() {
-//   delay(10);
-//   Mqtt_Loop();
-// }
-
-
-#include "hcsr04.h"
-
-void setup()
-{
-  setup_hcsr04();
+void setup() {
+  Serial.begin(115200);
+  BH1750FVI_Init();
+  DHT11_Init();
+  Mqtt_Init();
+  if (!client.connected()) {
+    Mqtt_Reconnect();
+  }
+  ticker.attach(10.0, MqttSend);
 }
 
-void loop()
-{
-  hcsr04_run();
+void loop() {
+  delay(10);
+  Mqtt_Loop();
 }
+
